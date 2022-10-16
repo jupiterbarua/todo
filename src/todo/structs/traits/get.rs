d0ad1f68922bb1fbe6e63238cce6b69a81fd3ec0
Jupiter <j.barua@metru.de>
2022-10-16 08:03:34 +0600

    Trait Update

--- src/todo/structs/traits/get.rs
+++ src/todo/structs/traits/get.rs
@@ -3,7 +3,14 @@ use serde_json::value::Value;
 
 pub trait Get {
     fn get(&self, title: &str, state: &Map<String, Value>) {
-        let item: 
+        let item: Option<&Value> = state.get(title);
+        match item {
+            Some(result) => {
+                println!("\n\nItem: {}", title);
+                println!("\n\nStatus: {}", result);
+            }, 
+            None =>  println!("Item: {} was not found", title)
+        }
 
         println!("{} is being fetched", title);
     }
